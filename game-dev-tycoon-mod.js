d74556335e5474a00921b23732ffe6c77a27ed24
Turntablelover <darthvader438@gmail.com>
2013-11-08 17:19:31 -0500

    0.5.0

--- game-dev-tycoon-mod.js
+++ game-dev-tycoon-mod.js
@@ -153,7 +153,7 @@ var eventId = "F413453F-2119-4969-A991-A8E99D6DEED7";//any string, but needs to 
 var myRandomEvent = {
 	id: eventId,
 	isRandom: true, //if you want to test this event, you can set this to false and it will trigger during dev. of the first game.
-	maxTriggers: 1,
+	maxTriggers: 4,
 	trigger: function (company) {
 		//only in second office and only while a game is in development.
 		//most events that fire during game-dev work better if they don't fire right at the start or right at the end, that's why we use isGameProgressBetween
@@ -222,6 +222,80 @@ var myRandomEvent = {
 
 GDT.addEvent(myRandomEvent);
 
+var eventId = "F514463G-2222-5000-B111-A9F11D7DEED7";//any string, but needs to be globally unique
+
+var myRandomEvent = {
+	id: eventId,
+	isRandom: true, //if you want to test this event, you can set this to false and it will trigger during dev. of the first game.
+	maxTriggers: 2,
+	trigger: function (company) {
+		//only in second office and only while a game is in development.
+		//most events that fire during game-dev work better if they don't fire right at the start or right at the end, that's why we use isGameProgressBetween
+		return company.currentLevel == 3 && company.isGameProgressBetween(0.2, 0.9);
+	},
+	//because we dynamically create the notification every time the event triggers, we use getNotification
+	getNotification: function (company) {
+		var game = company.currentGame;
+
+		var msg = "Today is National Charlie Day!{n} Charlie and the other Greenheart Games developers have been celebrating National Charlie Day, and they invite you to the special parade. They have also offered you Charlie's favourite type of cookies to give to you at the parade.\nDo you want to go to the parade? \n\nYou could work on your game instead, go to the parade, or don't go to the parade."
+			.localize().format(game.title);
+		//notice how we break up the story in two screens by using {n}. This creates a better flow than having one longer block of text.
+		//Also, since this is a story with multiple options and the buttons can only hold a small amount of text, we explain the options beforehand.
+
+		//the special parade, lowered a bit of hype on your game.
+		//since the event isn't delayed we can do this directly on the company, otherwise we could call adjustHype() on the notification object to schedule the effect with the notification.
+		company.adjustHype(5 - 20 * company.getRandom());//lower hype between 5 and -15.
+
+		return new Notification({
+			sourceId: eventId,//this is important, otherwise nothing will happen when the player selects an option.
+			header: "National Charlie Day!".localize(),
+			text: msg,
+			options: ["Work on Game Instead", "Go to Parade", "Don't go to Parade"]//maximum of three choices
+		});
+	},
+	complete: function (decision) {
+		//decision is a number and will correspond to the index of the option that was chosen.
+		//0=work on game instead, 1=go to parade, 2=don't go to parade
+		//it's best if every decision has a different outcome
+
+		var company = GameManager.company;//we fetch the company object for use later.
+
+		if (decision === 0) {//eat cake
+			//we create a new, simple notification to tell the outcome. no sourceId or options are necessary this time.
+			var n = new Notification({
+				header: "National Charlie Day!".localize(),//keep the header consistent with the prior part of the story
+				text: "You worked on the game instead. You disappointed the Greenheart Games developers."
+			});
+			n.adjustHype(5 + 25 * company.getRandom());//increase hype between 5 and 30.
+			
+			company.activeNotifications.addRange(n.split()); //since this notification should be shown immediately (not one second later) we insert it into activeNotifications. calling .split() is just good practice in case we use {n} inside the notification.
+			return;
+		}
+		if (decision === 1) {//go to parade
+			//nothing happens at first, but in a few weeks, you enjoy the parade
+			var n = new Notification({
+				header: "National Charlie Day!".localize(),
+				text: "You go to the parade, and you enjoy Charlie's favourite cookies\n Your game gets delayed!(-700 cr.) ",
+				weeksUntilFired: 1 + 2 * company.getRandom()
+			});
+			n.adjustCash(-700, "lose money on cookies");
+			company.notifications.push(n);//this notification isn't shown immediately so we add it to the normal company.notifications array.
+			return;
+		}
+		if (decision === 2) {//don't go to parade
+			var n = new Notification({
+				header: "National Charlie Day!".localize(),//keep the header consistent with the prior part of the story
+				text: "You don't go to the parade. You disappoint everyone at the parade including the Greenheart Games developers, while you work on your game instead."
+			});
+			n.adjustHype(5 + 20 * company.getRandom());//increase hype between 5 and 25
+			company.activeNotifications.addRange(n.split()); //since this notification should be shown immediately (not one second later) we insert it into activeNotifications. calling .split() ist just good practice in case we use {n} inside the notification.
+			return;
+		}
+	}
+};
+
+GDT.addEvent(myRandomEvent);
+
 //Platforms
 var icon = 'mods/Game-Dev-Tycoon-Mod/images/SuperBox.png';
 GDT.addPlatform({
@@ -273,7 +347,36 @@ GDT.addPlatform({
 			getNotification : function (company) {
 				return new Notification({
 					header : "Industry News".localize(),
-					text : "Supersonic Games has announced that they are publishing a shiny, new game console called the SuperStar {0}. It has a cool new feature where it has 2 CD Drives. You can now play your favourite games and listen to music at the same time.".localize().format(General.getETADescription('1/2/1', '1/3/4')),
+					text : "Supersonic Games has announced that they are publishing a shiny, new game console called the SuperStar {0}. It has a cool new feature where it has 2 CD Drives. You can now play your favourite games and listen to music at the same time.".localize().format(General.getETADescription('5/3/4', '6/1/1')),
+					image : icon
+				});
+			}
+		}
+	]
+});
+
+var icon = 'mods/Game-Dev-Tycoon-Mod/images/2GS.png';
+GDT.addPlatform({
+	id : 'Ninvento 2GS',
+	name : 'Ninvento 2GS',
+	company : 'Ninvento',
+	startAmount : 0.20,
+	unitsSold : 0.400,
+	licencePrize : 100000,
+	published : '22/4/4',
+	platformRetireDate : '40/4/4',
+	developmentCosts : 200000,
+	genreWeightings : [0.9, 1, 1, 0.9, 1, 0.8],
+	audienceWeightings : [1.0, 1.0, 0.6],
+	techLevel : 7,
+	iconUri : icon,
+	events : [{
+			id : '10548DA3-59F23-4F25-8855-F1E2732945BF',
+			date : '21/4/4',
+			getNotification : function (company) {
+				return new Notification({
+					header : "Industry News".localize(),
+					text : "Ninvento has announced that they are publishing a skinnier version of their popular hand-held console, The Ninvento 3DS {0}. The Ninvento 2DS is basically the 3DS with no 3D mode.".localize().format(General.getETADescription('21/2/3', '21/4/4')),
 					image : icon
 				});
 			}
@@ -297,6 +400,31 @@ GDT.addTopics([
 	} , {
 		id: "Chess", 
 		name: "Chess".localize("game topic"), 
+		genreWeightings: [0.6, 0.6, 0.6, 1.0, 1.0, 1.0], 
+		audienceWeightings: [1.0, 1.0, 0.6]
+	} , {
+		id: "Alternate Reality", 
+		name: "Alternate Reality".localize("game topic"), 
+		genreWeightings: [0.7, 0.7, 0.7, 1.0, 1.0, 1.0], 
+		audienceWeightings: [1.0, 1.0, 0.8]
+	} , {
+		id: "Japanese", 
+		name: "Japanese".localize("game topic"), 
+		genreWeightings: [0.8, 0.8, 1.0, 0.9, 0.9, 0.8], 
+		audienceWeightings: [1.0, 1.0, 0.6]
+	} , {
+		id: "Drinking", 
+		name: "Drinking".localize("game topic"), 
+		genreWeightings: [0.6, 0.6, 0.6, 1.0, 1.0, 1.0], 
+		audienceWeightings: [0.6, 0.6, 1.0]
+	} , {
+		id: "Math", 
+		name: "Math".localize("game topic"), 
+		genreWeightings: [0.6, 0.6, 0.6, 1.0, 1.0, 1.0], 
+		audienceWeightings: [1.0, 1.0, 0.6]
+	} , {
+		id: "Party", 
+		name: "Party".localize("game topic"), 
 		genreWeightings: [0.6, 0.6, 0.6, 1.0, 1.0, 1.0], 
 		audienceWeightings: [1.0, 1.0, 0.6]
 	} , {
